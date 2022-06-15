6bc5eb98efdd2c4dbe3cd2f176495bb8f52acbb1
LucieLavickova-Tesena <lucie.lavickova@tesena.com>
2022-06-15 15:04:16 +0200

    added warning with healenium report link to RF log.html

--- src/healenium_log.py
+++ src/healenium_log.py
@@ -1,6 +1,10 @@
+from robot.api.deco import keyword
 import docker
-client = docker.from_env()
-print(client.containers.get('f4601503b060').logs()) 
-## TODO: replace the container ID with container name or other way how to identify the "hlm-proxy" container for easier setup
-## TODO: extract the sentence "Report available at http://..." from the log output and write it to RobotFW log.html file on the test suite level (on the top)
-## TODO: try to write info about the healed locator in the RobotFW log.html file on the place where the element was used (on the test step level)
+import regex
+
+@keyword
+def getHealeniumReportLink():
+    client = docker.from_env()
+    logs = client.containers.get('hlm-proxy').logs()
+    reportlink = regex.search(r"(?r)Report available at .*", logs.decode("utf-8")) # find last occurence of this regex
+    return reportlink[0]
